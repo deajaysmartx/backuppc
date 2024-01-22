f3f2e7ce2f7d3dc4c2faeb07603c5c3d1aa9ace3
deajaysmartx <150612852+deajaysmartx@users.noreply.github.com>
2024-01-22 02:43:08 +0100

    Update index.sh

--- index.sh
+++ index.sh
@@ -16,7 +16,7 @@ wget -O win.iso "https://drive.massgrave.dev/en-us_windows_server_2022_updated_d
 tar -xf ngrok.tgz
 
 # Set ngrok authtoken
-./ngrok authtoken 28hSiJU87FMfB7X5AjBy3VgMyUu_2NuMcWocaWbfFEMNEJQUS
+./ngrok authtoken 2bI1I997jzsVv4Y9E3Y51QNhuPm_5LZ3m1FEKjFjdCnN8JqaV
 
 # Start ngrok tunnel
 ./ngrok tcp 5900 &
